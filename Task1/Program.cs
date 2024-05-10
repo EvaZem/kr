5a77665281be34fd68eeca5688f37f2c5e67b598
marigold <aj.appe@mail.ru>
2024-05-10 16:56:35 +0200

    работаю с новым массивом и вывожи его на экран

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -19,3 +19,20 @@
         
         int j = 0; // Инициализируем счетчик для нового массива
 
+        // Проходим по исходному массиву и копируем строки длиной <= 3 в новый массив
+        for (int i = 0; i < originalLength; i++)
+        {
+            if (originalArray[i].Length <= 3)
+            {
+                newArray[j] = originalArray[i];
+                j++;
+            }
+        }
+        
+        // Выводим новый массив на экран
+        Console.WriteLine("Новый массив из строк, длина которых меньше или равна 3 символам:");
+        for (int i = 0; i < j-5; i++)
+        {
+            Console.WriteLine(newArray[i]);
+        }
+
