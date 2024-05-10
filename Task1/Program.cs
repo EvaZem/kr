d1971d4df3805b1c14e3ac0681ee74ab17029bfa
marigold <aj.appe@mail.ru>
2024-05-10 16:52:50 +0200

    работаю с исходным массивом

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -5,3 +5,12 @@
             Console.WriteLine(str);
         }
     }
+        // Инициализируем исходный массив строк
+        string[] originalArray = { "sdf", "huk", "qwc", "bvd", "zx", "x" };
+        // Печатаем массив
+        Console.WriteLine("Изначальный массив:");
+        PrintArray(originalArray); 
+        
+        // Подсчитываем количество строк в исходном массиве
+        int originalLength = originalArray.Length;
+
